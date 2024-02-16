cd957e83b9d782d7aac9e25c56d6bf802477666f
GideonAg <gideonagbosu72@gmail.com>
2024-02-16 13:42:42 +0000

    Changed stage to production

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,7 @@
 import merge from "lodash.merge";
 
 process.env.NODE_ENV = process.env.NODE_ENV || "development";
-const stage = process.env.STAGE || "local";
+const stage = process.env.STAGE || "production";
 
 let envConfig;
 
